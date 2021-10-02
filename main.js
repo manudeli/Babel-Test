48c95c545428764cab7637d97d49407676102d28
Jonghyeon Ko <flowithusdev@gmail.com>
2021-10-03 02:32:36 +0900

    vue boiler plage

--- main.js
+++
@@ -1,9 +0,0 @@
-import './main.css'
-
-const c = {}
-const d = 3
-
-const increase = (num) => num + 1
-
-const arr = []
-console.log(arr.includes('a'))
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  plugins: [require('autoprefixer')],
+module.export = {
+  plugin: [require('autoprefixer')],
 }
